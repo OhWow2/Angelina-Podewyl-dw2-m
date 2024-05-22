bd623fe30ac239cfa33e8344098811976bbe0e10
OhWow2 <Podewylla@gmail.com>
2024-05-22 18:08:15 -0300

    «actualizacion»

--- script.js
+++ script.js
@@ -1,4 +1,39 @@
-let nombreDeProducto = "Te"
-let Precio = 217
-prompt 
+let nombre;
+let email;
+let contraseña;
 
+function registroUsuario(){
+    nombre = prompt("¿Cuál es tu nombre?")
+    email= prompt("¿Cuál es tu email?")
+    contraseña= prompt("¿Cuál es tu contraseña?")
+    alert("Su nombre es: " + nombre + " " + email)
+}
+registroUsuario ()
+let emailIngresado;
+let contraseñaIngresado;
+
+function inicioSesion(){
+    emailIngresado= prompt("Ingrese el Email")
+    contraseñaIngresado= prompt("Ingrese la contraseña")
+    if(email == emailIngresado && contraseña ==contraseñaIngresado){
+        alert("Hola Bienvenido " + nombre)
+    }else{
+        alert("Ingrese un GMAIL o Contraseña valida")
+    }
+}
+inicioSesion()
+
+let contraseñaNueva
+function actualizarContraseña(){
+    emailIngresado= prompt("Ingrese el Email")
+    contraseña = prompt("Ingrese la contraseña")
+    if(email== emailIngresado && contraseña ==contraseñaIngresado){
+        contraseñaNueva= prompt("Ingrese la nueva contraseña")
+        contraseña = contraseñaNueva;
+        alert(contraseña);
+    }else{
+        alert("Ingrese un GMAIL o Contraseña valida")
+    }
+    
+}
+actualizarContraseña()
