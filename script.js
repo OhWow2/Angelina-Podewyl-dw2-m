3dff4e315e8f9b22904bb3273911a5126af4f091
Angelina Podewyl <109753651+OhWow2@users.noreply.github.com>
2024-06-09 20:36:55 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,27 +1,41 @@
 let nombre;
 let email;
 let contraseña;
+let usuarios = [];
 
 function registroUsuario(){
     nombre = prompt("¿Cuál es tu nombre?")
     email= prompt("¿Cuál es tu email?")
     contraseña= prompt("¿Cuál es tu contraseña?")
-    alert("Su nombre es: " + nombre + " " + email)
+    alert("Su nombre es: " + nombre + " " + email);
+    let nuevoUsuario ={
+        usuario:nombre,
+        email:email,
+        contraseña:contraseña
+    }
+    usuarios.push(nuevoUsuario)
+    console.log (nuevoUsuario)
+
 }
-registroUsuario ()
+registroUsuario()
+
 let emailIngresado;
 let contraseñaIngresado;
+let nombreIngresado
 
 function inicioSesion(){
+    nombreIngresado= prompt ("Ingrese su nombre")
     emailIngresado= prompt("Ingrese el Email")
     contraseñaIngresado= prompt("Ingrese la contraseña")
-    if(email == emailIngresado && contraseña ==contraseñaIngresado){
+
+    if(email == emailIngresado && contraseña ==contraseñaIngresado && nombre==nombreIngresado){
         alert("Hola Bienvenido " + nombre)
+        window.location.href= 'PaginaPrincipal.html'
     }else{
         alert("Ingrese un GMAIL o Contraseña valida")
     }
 }
-inicioSesion()
+
 
 let contraseñaNueva
 function actualizarContraseña(){
@@ -36,4 +50,3 @@ function actualizarContraseña(){
     }
     
 }
-actualizarContraseña()
